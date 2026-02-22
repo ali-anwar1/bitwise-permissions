e8ab6792d7a75528b563655859cb066014cf60f6
Ali Anwar <dev.alianwar@gmail.com>
2026-02-22 11:16:58 +0500

    permissions defined

---
+++ index.js
@@ -0,0 +1,80 @@
+// Create, Read, Update and Delete files permissions
+// 00000100
+// 00000101
+// 00000110
+// 00000111
+
+const createPermission = 4;
+const readPermission = 2;
+const updatePermission = 1;
+const deletePermission = 0;
+
+function hasPermission(permission, permissionToCheck) {
+    return (permission & permissionToCheck) === permissionToCheck;
+}
+
+const userPermissions = {
+    default: 0,
+    createAndRead: createPermission | readPermission,
+    createAndUpdate: createPermission | updatePermission,
+    readAndDelete: readPermission | deletePermission,
+    createReadUpdate: createPermission | readPermission | updatePermission,
+    '*': createPermission | readPermission | updatePermission | deletePermission,
+}
+
+function toBinary(permissions) {
+    const bitsArray = permissions.toString(2).split('')
+    if (bitsArray.length < 8) {
+        bitsArray.unshift('0'.repeat(8 - bitsArray.length));
+    }
+    return bitsArray.join('');
+}
+
+for (const key in userPermissions) {
+    console.log(toBinary(userPermissions[key]), key, userPermissions[key]);
+}
+
+
+
+const exampleUsers = [
+    { name: 'John', permission: userPermissions.default, expected: 'No permissions' },
+    { name: 'Alice', permission: userPermissions.createAndRead, expected: 'Create and Read permissions' },
+    { name: 'Bob', permission: userPermissions.createAndUpdate, expected: 'Create and Update permissions' },
+    { name: 'Charlie', permission: userPermissions.readAndDelete, expected: 'Read and Delete permissions' },
+    { name: 'Dave', permission: userPermissions.createReadUpdate, expected: 'Create, Read and Update permissions' },
+    { name: 'Eve', permission: userPermissions['*'], expected: 'All permissions' },
+];
+
+
+const testUser = (user) => {
+    switch (user.permission) {
+        case userPermissions['*']:
+            return `All permissions`;
+        case userPermissions.createReadUpdate:
+            return `Create, Read and Update permissions`;
+        case userPermissions.createAndRead:
+            return `Create and Read permissions`;
+        case userPermissions.createAndUpdate:
+            return `Create and Update permissions`;
+        case userPermissions.readAndDelete:
+            return `Read and Delete permissions`;
+        case userPermissions.default:
+            return `No permissions`;
+        default:
+            return `Unknown permissions`;
+    }
+}
+
+const testUsers = (testUsers) => {
+    testUsers.forEach(user => {
+        if (testUser(user) !== user.expected) {
+            console.error(`Test failed for ${user.name}. Expected: ${user.expected}, Got: ${testUser(user)}`);
+        } else {
+            console.log(`Test passed for ${user.name}`);
+        }
+    });
+}
+
+testUsers(exampleUsers);
+
+
